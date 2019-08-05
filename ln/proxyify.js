8f46d555ce0f38cbfe3f010455c7a30056686ad2
muggy8 <muggy8@gmail.com>
2019-08-05 16:20:12 -0700

    better compatibility with other node versions

--- ln/proxyify.js
+++ ln/proxyify.js
@@ -1,4 +1,5 @@
 let fs = require("fs")
+const waitFor = (fn, ...args)=>new Promise((accept, reject)=>fn.apply(this, [...args, (uwu, owo)=>uwu ? reject(uwu) : accept(owo)]))
 module.exports = async function(document, window){
 	await RAFP()
 	await RAFP()
@@ -105,7 +106,7 @@ module.exports = async function(document, window){
 
 	Array.prototype.forEach.call(document.querySelectorAll("[class]"), el=>!el.getAttribute("class") && el.removeAttribute("class"))
 
-	let terms = await fs.promises.readFile("./terms.json", "utf-8").then(owo=>JSON.parse(owo))
+	let terms = await waitFor(fs.readFile, "./terms.json", "utf-8").then(owo=>JSON.parse(owo))
 	await RAFP()
 	await RAFP()
 
