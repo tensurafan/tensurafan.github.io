9dcc45019c1312608dd1c77452edc594b27277c0
Muggy Ate <muggy8@gmail.com>
2020-09-09 21:53:01 -0700

    added buttons to set terms

--- js/chooseables-config/controller.js
+++ js/chooseables-config/controller.js
@@ -4,8 +4,41 @@
 	let view = proxymity(template, {
 		termsChosen,
 		termsOptions,
-		termsList: Object.keys(termsOptions)
+		termsList: Object.keys(termsOptions),
+		setOfficial: function(){
+			view.app.termsList.forEach(term=>{
+				let officialTermIndex = termsOptions[term].officialTermIndex
+				termsChosen[term] = termsOptions[term].options[officialTermIndex]
+			})
+			app.saveSettings()
+		},
+		setJank: function(){
+			view.app.termsList.forEach(term=>{
+				let lastIndex = termsOptions[term].options.length - 1
+				termsChosen[term] = termsOptions[term].options[lastIndex]
+			})
+			app.saveSettings()
+		},
+		setRandom: function(){
+			view.app.termsList.forEach(term=>{
+				let lastIndex = termsOptions[term].options.length - 1
+				let randomIndex = randomInt(lastIndex)
+				termsChosen[term] = termsOptions[term].options[randomIndex]
+			})
+			app.saveSettings()
+		},
+		setDefault: function(){
+			view.app.termsList.forEach(term=>{
+				termsChosen[term] = termsOptions[term].options[0]
+			})
+			app.saveSettings()
+		},
 	})
+
+	function randomInt(max = 1){
+		max += .999999999
+		return Math.floor(Math.random() * max)
+	}
 
 	routerInstance.add("/chooseables", view)
 
